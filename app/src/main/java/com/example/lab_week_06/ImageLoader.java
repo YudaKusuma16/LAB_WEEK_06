ef946955f9ffc8e5fd908fb75c503030f901be38
Yuda Kusuma <yuda.kusuma@student.umn.ac.id>
2025-10-05 01:25:47 +0700

    Commit No.1 - Adding a RecyclerView and Populating it

---
+++ app/src/main/java/com/example/lab_week_06/ImageLoader.java
@@ -0,0 +1,10 @@
+package com.example.lab_week_06;
+
+import android.widget.ImageView;
+
+import android.widget.ImageView;
+
+public interface ImageLoader {
+    void loadImage(String imageUrl, ImageView imageView);
+}
+
